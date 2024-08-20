5e3959771f1a0b5e3e75d6a5ec53c5fb3f540432
extrymes <simon.abras@gmail.com>
2024-08-20 11:46:34 +0200

    chore: update philo.h

--- philo/philo.h
+++ philo/philo.h
@@ -6,15 +6,17 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:43:51 by sabras            #+#    #+#             */
-/*   Updated: 2024/08/10 16:24:09 by sabras           ###   ########.fr       */
+/*   Updated: 2024/08/20 11:34:34 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
+# include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
 # include <pthread.h>
+# include <sys/time.h>
 # define RED "\x1b[31m"
 # define GREEN "\x1b[32m"
 # define YELLOW "\x1b[33m"
@@ -25,25 +27,36 @@
 
 typedef struct s_data
 {
-	int	nb_of_philos;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	nb_of_each_philo_to_eat;
+	int				nb_philos;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				nb_of_each_philo_to_eat;
+	int				must_eat;
+	int				total_eat;
+	unsigned long	time;
 }	t_data;
 
 typedef struct s_philo
 {
-	int			id;
-	pthread_t	thread;
+	int				id;
+	int				eat;
+	pthread_t		thread;
+	pthread_mutex_t	*fork_r;
+	pthread_mutex_t	*fork_l;
+	pthread_mutex_t	fork_m;
+	unsigned long	last_meal;
+	t_data			*data;
 }	t_philo;
 
 // Routine
-void	*routine(void	*ph);
+void			*routine(void	*ph);
 
 // Utils
-void	ft_init_philos(int nb_of_philos);
-int		ft_atoi(char *s);
-void	ft_throw_error(char *s);
+int				ft_atoi(char *s);
+void			ft_dest_mutex(int nb_philos, t_philo *philos);
+unsigned long	ft_current_time(void);
+unsigned long	ft_elapsed_time(unsigned long time);
+void			ft_throw_error(char *s);
 
 #endif
