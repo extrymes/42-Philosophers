bbbc7d7bd56d5d28589f182d7f6795d8428d6f5c
extrymes <simon.abras@gmail.com>
2024-08-20 11:44:17 +0200

    feat: add `ft_dest_mutex()` and time functions

--- philo/src/utils.c
+++ philo/src/utils.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:47:54 by sabras            #+#    #+#             */
-/*   Updated: 2024/08/10 16:27:13 by sabras           ###   ########.fr       */
+/*   Updated: 2024/08/20 10:19:00 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,39 @@ int	ft_atoi(char *s)
 	if (n == 0)
 		return (ft_throw_error("arguments must be positive numbers"), 0);
 	return (n);
+}
+
+void	ft_dest_mutex(int nb_philos, t_philo *philos)
+{
+	int	i;
+
+	i = 0;
+	while (i < nb_philos)
+	{
+		pthread_mutex_destroy(philos[i].fork_r);
+		pthread_mutex_destroy(philos[i].fork_l);
+		pthread_mutex_destroy(&philos[i].fork_m);
+		i++;
+	}
+}
+
+unsigned long	ft_current_time(void)
+{
+	struct timeval	time;
+	unsigned long	total;
+	unsigned long	sec;
+	unsigned long	u_sec;
+
+	gettimeofday(&time, NULL);
+	sec = (time.tv_sec * 1000);
+	u_sec = (time.tv_usec / 1000);
+	total = sec + u_sec;
+	return (total);
+}
+
+unsigned long	ft_elapsed_time(unsigned long time)
+{
+	return (ft_current_time() - time);
 }
 
 void	ft_throw_error(char *s)
