9b7c9fe7198bb1b4f57a83d26c7447cc372862df
extrymes <simon.abras@gmail.com>
2024-08-20 11:33:15 +0200

    feat: add all steps to the routine

--- philo/src/routine.c
+++ philo/src/routine.c
@@ -6,17 +6,45 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/07 15:30:43 by sabras            #+#    #+#             */
-/*   Updated: 2024/08/10 16:24:56 by sabras           ###   ########.fr       */
+/*   Updated: 2024/08/20 11:09:21 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../philo.h"
+
+void	ft_print(t_philo *philo, char *is_doing);
 
 void	*routine(void	*ph)
 {
 	t_philo	*philo;
 
 	philo = (t_philo *)ph;
-	printf("philo%d\n", philo->id);
-	return (0);
+	if (philo->id % 2 == 0)
+		usleep(1000);
+	philo->last_meal = ft_current_time();
+	while (1)
+	{
+		pthread_mutex_lock(philo->fork_r);
+		ft_print(philo, "has taken a fork 🍴");
+		pthread_mutex_lock(philo->fork_l);
+		ft_print(philo, "has taken a fork 🍴");
+		philo->eat++;
+		if (philo->eat == philo->data->must_eat)
+			philo->data->total_eat++;
+		philo->last_meal = ft_current_time();
+		ft_print(philo, "is eating 🍝");
+		usleep(philo->data->time_to_eat * 1000);
+		pthread_mutex_unlock(philo->fork_r);
+		pthread_mutex_unlock(philo->fork_l);
+		ft_print(philo, "is sleeping 💤");
+		usleep(philo->data->time_to_sleep * 1000);
+		ft_print(philo, "is thinking 💭");
+	}
+	return (NULL);
+}
+
+void	ft_print(t_philo *philo, char *is_doing)
+{
+	printf("%lums\t%d %s\n", ft_elapsed_time(philo->data->time),
+		philo->id + 1, is_doing);
 }
