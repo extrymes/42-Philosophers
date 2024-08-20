afea5c11155886301943cced41d39fd06fce835b
extrymes <simon.abras@gmail.com>
2024-08-20 11:46:05 +0200

    chore: set default values for data structure and clean code

--- philo/philo.c
+++ philo/philo.c
@@ -6,7 +6,7 @@
 /*   By: sabras <sabras@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 10:24:47 by sabras            #+#    #+#             */
-/*   Updated: 2024/08/10 16:29:08 by sabras           ###   ########.fr       */
+/*   Updated: 2024/08/20 11:41:14 by sabras           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,21 @@ t_philo	*ft_init_philos(t_data *data);
 int	main(int ac, char **av)
 {
 	t_data	data;
+	t_philo	*philos;
 
 	if (ac != 5 && ac != 6)
-		return (printf(RED "Usage:\n\t./philo <nb_of_philos> <time_to_die> \
+		return (printf(RED "Usage:\n\t./philo <nb_philos> <time_to_die> \
 <time_to_eat> <time_to_sleep> <nb_of_each_philo_to_eat>\n" RESET), 1);
-	data.nb_of_philos = ft_atoi(av[1]);
+	data.nb_philos = ft_atoi(av[1]);
 	data.time_to_die = ft_atoi(av[2]);
 	data.time_to_eat = ft_atoi(av[3]);
 	data.time_to_sleep = ft_atoi(av[4]);
 	if (av[5])
 		data.nb_of_each_philo_to_eat = ft_atoi(av[5]);
-	ft_init_philos(data.nb_of_philos);
+	data.must_eat = 0;
+	data.total_eat = 0;
+	data.time = ft_current_time();
+	philos = ft_init_philos(&data);
 	return (0);
 }
 
